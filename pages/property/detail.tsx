ed908fe4d6363fe85a284d1ace6554033b2e82d2
felix-824 <nf94new@gmail.com>
2026-09-24 15:40:57 +0900

    feat: create new comment process aligned to chosen Property

--- pages/property/detail.tsx
+++ pages/property/detail.tsx
@@ -30,8 +30,8 @@ import 'swiper/css/pagination';
 import { GET_COMMENTS, GET_PROPERTIES, GET_PROPERTY } from '../../apollo/user/query';
 import { T } from '../../libs/types/common';
 import { Direction, Message } from '../../libs/enums/common.enum';
-import { LIKE_TARGET_PROPERTY } from '../../apollo/user/mutation';
-import { sweetMixinErrorAlert, sweetTopSmallSuccessAlert } from '../../libs/sweetAlert';
+import { CREATE_COMMENT, LIKE_TARGET_PROPERTY } from '../../apollo/user/mutation';
+import { sweetErrorHandling, sweetMixinErrorAlert, sweetTopSmallSuccessAlert } from '../../libs/sweetAlert';
 
 SwiperCore.use([Autoplay, Navigation, Pagination]);
 
@@ -60,6 +60,7 @@ const PropertyDetail: NextPage = ({ initialComment, ...props }: any) => {
 
 	/** APOLLO REQUESTS **/
    const [likeTargetProperty] = useMutation(LIKE_TARGET_PROPERTY);
+   const [createComment] = useMutation(CREATE_COMMENT);
    
 
 	const {
@@ -183,6 +184,20 @@ const {
 		commentInquiry.page = value;
 		setCommentInquiry({ ...commentInquiry });
 	};
+
+	 const createCommentHandler = async () => {
+	try {
+		if (!user._id) throw new Error(Message.NOT_AUTHENTICATED);
+
+		await createComment({ variables: { input: insertCommentData } });
+
+		setInsertCommentData({ ...insertCommentData, commentContent: '' });
+
+		await getCommentsRefetch({ input: commentInquiry });
+	} catch (err: any) {
+		await sweetErrorHandling(err);
+	}
+};
 
 	if (device === 'mobile') {
 		return <div>PROPERTY DETAIL PAGE</div>;
@@ -496,6 +511,7 @@ const {
 										<Button
 											className={'submit-review'}
 											disabled={insertCommentData.commentContent === '' || user?._id === ''}
+											onClick={createCommentHandler}
 										>
 											<Typography className={'title'}>Submit Review</Typography>
 											<svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 17 17" fill="none">
